249eb8d9a147f399519a97a4475e2e7c748b0738
Jeremiah <jdporcu@cpp.edu>
2015-02-15 09:22:20 -0800

    Final Edit
    Finally got MVN to build as a package. Tests are working both in maven
    and in eclipse.

--- src/test/java/edu/csupomona/cs480/ParkedUserTest.java
+++ src/test/java/edu/csupomona/cs480/ParkedUserTest.java
@@ -1,6 +1,5 @@
 package edu.csupomona.cs480;
 
-
 import static org.junit.Assert.*;
 
 import org.junit.Before;
@@ -10,51 +9,51 @@ import edu.csupomona.cs480.data.ParkedUser;
 
 public class ParkedUserTest {
 
-	public class ParkUserTest {
-		private ParkedUser testParked;
-		private ParkUserTest(){
-			testParked = new ParkedUser();
-			testParked.setUserName("sang");
-			testParked.setContactInfo("sang@yahoo.com");
-			testParked.setHour(2);
-			testParked.setMinute(30);
-			testParked.setPickup(true);
-			testParked.setPickUpBuildingNum(8);
-		}
-		
-		@Before
-		public void parkUserShouldInsertPickUpFalse(){
-			assertEquals(false, testParked.getPickup());
-		}
-		@Test
-		public void parkUserShouldInsertParkedUser(){
-			String name = testParked.getUserName();
-			assertEquals("sang", name);
-		}
-		@Test
-		public void parkUserShouldInsertHour(){
-			
-			assertEquals(2, testParked.getHour());
-		}
-		
-		@Test
-		public void parkUserShouldInsertMinute(){
-			
-			assertEquals(30, testParked.getMinute());
-		}
-		
-		@Test
-		public void parkUserShouldInsertPickUp(){
-			
-			assertEquals(true, testParked.getPickup());
-		}
-		@Test
-		public void parkUserShouldInsertBuildingNum(){
-			
-			assertEquals(8, testParked.getPickUpBuildingNum());
-		}
-		
-		
+	private ParkedUser testParked;
+
+	public ParkedUserTest() {
+		testParked = new ParkedUser();
+		testParked.setUserName("sang");
+		testParked.setContactInfo("sang@yahoo.com");
+		testParked.setHour(2);
+		testParked.setMinute(30);
+		testParked.setPickup(true);
+		testParked.setPickUpBuildingNum(8);
+	}
+//
+	//@Before
+	//public void parkUserShouldInsertPickUpFalse() {
+	//	assertEquals(false, testParked.getPickup());
+	//}
+
+	@Test
+	public void parkUserShouldInsertParkedUser() {
+		String name = testParked.getUserName();
+		assertEquals("sang", name);
+	}
+
+	@Test
+	public void parkUserShouldInsertHour() {
+
+		assertEquals(2, testParked.getHour());
+	}
+
+	@Test
+	public void parkUserShouldInsertMinute() {
+
+		assertEquals(30, testParked.getMinute());
+	}
+
+	@Test
+	public void parkUserShouldInsertPickUp() {
+
+		assertEquals(true, testParked.getPickup());
+	}
+
+	@Test
+	public void parkUserShouldInsertBuildingNum() {
+
+		assertEquals(8, testParked.getPickUpBuildingNum());
 	}
 
 }
